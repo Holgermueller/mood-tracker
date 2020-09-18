991076e8c90961db57ef91d9def7675047f002e4
Holger Mueller <9jholger@gmail.com>
2020-09-18 11:45:54 -0500

    refactored some code

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -1,15 +1,9 @@
 <script>
   import MoodStore from "../stores/MoodStore";
-
-  let moods = [];
-
-  MoodStore.subscribe((data) => {
-    moods = data;
-  });
 </script>
 
 <div>
-  {#each moods as mood}
+  {#each $MoodStore as mood}
     <h5>{mood.mood}</h5>
     <h6>{mood.date}</h6>
   {/each}
