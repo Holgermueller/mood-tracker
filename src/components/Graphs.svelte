b61e8e1cd95c1801677f409bbc955ebccadd9b45
Holger Mueller <9jholger@gmail.com>
2020-09-26 21:29:19 -0500

    some styling withdnamic classes

--- src/components/Graphs.svelte
+++ src/components/Graphs.svelte
@@ -3,21 +3,38 @@
 </script>
 
 <style>
+  .graphs {
+    margin-bottom: 7px;
+  }
   .mood {
     background: #fafafa;
+    width: 75%;
     margin: 10px auto;
     position: relative;
+    border-radius: 15px;
   }
   span {
     display: inline-block;
     padding: 10px 20px;
-    border-radius: 15px;
+  }
+  .Bored {
+    background: rgba(69, 196, 150, 0.2);
+    border-left: 4px solid green;
+  }
+  .Sad {
+    background: rgba(0, 0, 255, 0.2);
+    border-left: 4px solid blue;
+  }
+
+  .Angry {
+    background: rgba(217, 27, 66, 0.2);
+    border-left: 2px solid red;
   }
 </style>
 
-<div>
+<div class="graphs">
   {#each $MoodStore as mood}
-    <div class="mood">
+    <div class="mood {mood.mood}">
       <span>{mood.mood} | Days felt: {mood.timesFelt} </span>
     </div>
   {/each}
