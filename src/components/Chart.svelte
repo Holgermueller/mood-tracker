8d8dff96cb859709f835c84c7906070e0120d4dd
Holger Mueller <9jholger@gmail.com>
2020-10-01 16:56:53 -0500

    began scaffold of pie chart

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -2,29 +2,30 @@
   import MoodStore from "../stores/MoodStore";
   import Chart from "chart.js";
 
-  $: instancesTracked = $MoodStore.reduce(
-    (a, { timesFelt }) => a + timesFelt,
-    0
-  );
-
-  $: MoodNames = $MoodStore.Map((mood) => mood.mood);
-
-  $: TimesFelt = $MoodStore.Map((mood) => mood.timesFelt);
+  console.log($MoodStore);
 
   let ctx = document.getElementById("PieChart");
 
+  const moodData = {};
+
   const PieChart = new Chart(ctx, {
     type: "pie",
-    cutoutPercentage: 0,
-    labels: [$MoodNames],
-    data: [$instancesTracked],
+    data: {
+      labels: ["mood1", "mood2", "mood3", "mood4", "mood5", "mood6"],
+      datasets: [
+        {
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: ["red", "blue", "aqua", "green", "black", "orange"],
+        },
+      ],
+    },
   });
 </script>
 
 <div>
   <h2>A chart will appear here.</h2>
 
-  <div id="PieChart" />
+  <canvas id="PieChart" width="400" height="400" />
   {#each $MoodStore as mood}
     <h5>{mood.mood}</h5>
     <h6>{mood.timesFelt}</h6>
