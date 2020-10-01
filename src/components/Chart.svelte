b3bdbbcd9061d4abbc6b63e57e3771dd8ef0620c
Holger Mueller <9jholger@gmail.com>
2020-10-01 13:42:45 -0500

    started working on pie chart

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -1,11 +1,32 @@
 <script>
   import MoodStore from "../stores/MoodStore";
+  import Chart from "chart.js";
+
+  $: instancesTracked = $MoodStore.reduce(
+    (a, { timesFelt }) => a + timesFelt,
+    0
+  );
+
+  $: MoodNames = $MoodStore.Map((mood) => mood.mood);
+
+  $: TimesFelt = $MoodStore.Map((mood) => mood.timesFelt);
+
+  let ctx = document.getElementById("PieChart");
+
+  const PieChart = new Chart(ctx, {
+    type: "pie",
+    cutoutPercentage: 0,
+    labels: [$MoodNames],
+    data: [$instancesTracked],
+  });
 </script>
 
 <div>
   <h2>A chart will appear here.</h2>
+
+  <div id="PieChart" />
   {#each $MoodStore as mood}
     <h5>{mood.mood}</h5>
-    <h6>{mood.date}</h6>
+    <h6>{mood.timesFelt}</h6>
   {/each}
 </div>
