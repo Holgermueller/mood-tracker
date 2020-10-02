4781ace3079f4363ac8b1e505bed455055ddbb60
Holger Mueller <9jholger@gmail.com>
2020-10-02 11:28:03 -0500

    got a pie chart on dom

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -1,33 +1,34 @@
 <script>
   import MoodStore from "../stores/MoodStore";
-  import Chart from "chart.js";
+  import Pie from "svelte-chartjs/src/Pie.svelte";
 
   console.log($MoodStore);
 
-  let ctx = document.getElementById("PieChart");
-
   const moodData = {};
 
-  const PieChart = new Chart(ctx, {
-    type: "pie",
-    data: {
-      labels: ["mood1", "mood2", "mood3", "mood4", "mood5", "mood6"],
-      datasets: [
-        {
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: ["red", "blue", "aqua", "green", "black", "orange"],
-        },
-      ],
-    },
-  });
+  let data = {
+    labels: ["mood1", "mood2", "mood3", "mood4", "mood5", "mood6"],
+    datasets: [
+      {
+        label: "Mood",
+        backgroundColor: [
+          "red",
+          "#0066ff",
+          "#00ffff",
+          "#33cc33",
+          "#000066",
+          "#ff9900",
+        ],
+        data: [12, 19, 3, 5, 2, 3],
+      },
+    ],
+  };
+
+  let options = {
+    responsive: true,
+  };
 </script>
 
 <div>
-  <h2>A chart will appear here.</h2>
-
-  <canvas id="PieChart" width="400" height="400" />
-  {#each $MoodStore as mood}
-    <h5>{mood.mood}</h5>
-    <h6>{mood.timesFelt}</h6>
-  {/each}
+  <Pie {data} {options} />
 </div>
